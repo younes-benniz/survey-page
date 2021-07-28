85e299b16bd8b0ea0bc1eafdfcd669aad5d81a59
Younes Benniz <bennizyouness@gmail.com>
2021-07-28 23:43:52 +0100

    Adding swagger

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request, jsonify
-import json
-app = Flask(__name__)
+import werkzeug
+werkzeug.cached_property = werkzeug.utils.cached_property
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+from flask_restplus import Api, Resource, fields
 
-@app.route("/")
+import json
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+@app.route("/survey")
 def hello_world():
     return render_template('survey.html')
 @app.route('/results', methods=['GET', 'POST'])
